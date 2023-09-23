7f50a3471f2d2a194ee9346dc2c4768806334a6a
wilkin <wilkintrinidad10@gmail.com>
2023-09-23 15:54:52 -0400

    add a funtionality to scalable for more lotery

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,84 @@
 
-fun main(args: Array<String>) {
-    //replica del sistema de Loteria
-    var loteriaCorrecta = false
-do {
-    val indice = Regex("[0-9]")
-    val indiceLoteria = presentarMenu()
-    println(indice)
-}while (false)
+fun main() {
+    val lotteryList = listOf("Leidsa", "National", "Real","lotopool")
+
+    println("Welcome to the lottery system by Leidsa. We have the following lotteries:")
+
+    // Display the list of lotteries with numeric indices
+    lotteryList.forEachIndexed { index, lottery ->
+        println("$index. $lottery")
+    }
+
+    // Ask the user to select a lottery
+    var selectedLottery: String? = null
+    while (selectedLottery == null) {
+        val indexStr = readLine()
+        val index = indexStr?.toIntOrNull()
+
+        if (index != null && index in 0 until lotteryList.size) {
+            selectedLottery = lotteryList[index]
+        } else {
+            println("Plid index.")
+        }
+    }
+    var CountLotery : Int? = when(selectedLottery) {
+        "Leidsa", "National", "Real" -> 3
+        "lotopool" -> 5
+        else -> null
+    }
+    if (CountLotery == null) {
+
+        println(" tu numero es nulo, cuidado")
+        return
+    }
+
+    val lista = mutableListOf<Int>()
+    //select you number
+    while(CountLotery > 0){
+        println("Hey seleccion tu jugada numero $CountLotery")
+        //obtengo la entrada y la comviero en numeros
+        var jugada = readln().toIntOrNull()
 
 
-}
-val listasDeLoterias = listOf("leidsa","nacional","real")
+        while (jugada == null){
+            println("no se permite numeros nulos")
+            jugada = readLine()?.toIntOrNull()
+        }
+        while ( jugada != null && (jugada > 99|| jugada < 0)){
+            println("Solo se permiten numeros del rangos")
+            jugada = readLine()?.toIntOrNull()
 
-fun presentarMenu() :String{
+        }
 
-    println("Bienvenido al sistema de loteria de leidsa, tenemos las siguientes loterias")
-    println("-------------------------------------------------------------------------------")
-    println("Porfabor seleccione el indice numerico para seleccionar una loteria")
+        if(jugada != null && jugada in 0 until 99){
+                lista.add(jugada)
+        }else {
+            println("algo salio mal o no estuviste en el rango de 0-99 o el numero fue nulo o solo puso letras")
+            continue
+        }
 
-    val listaImprimida = listasDeLoterias.map { "${listasDeLoterias.indexOf(it)} $it" }
-    listaImprimida.forEach { println(it) }
 
-    return readln()
+
+        CountLotery--
+
+    }
+    lista.run {sort()}
+
+
+    println("You have selected the lottery: $selectedLottery \n" +
+            "y estos son tus numeros $lista \nahora lanzaremos los numeros de la loteria ")
+
+    val listaDeNumerosGanadores = mutableListOf<Int>()
+       while (listaDeNumerosGanadores.size < lista.size){
+           val item = (0..99).random()
+           listaDeNumerosGanadores += item
+       }
+
+
+
+
+    listaDeNumerosGanadores.run { sorted() }
+    println(listaDeNumerosGanadores)
 
 }
 
